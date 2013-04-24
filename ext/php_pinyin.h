f5f4b929421daf6d7923bf7a04bc16c874bc282b
Gu Weigang <guweigang@outlook.com>
2013-04-24 22:09:04 +0800

    change declare of exact to exactConvert

--- ext/php_pinyin.h
+++ ext/php_pinyin.h
@@ -70,7 +70,7 @@ PHP_METHOD(Pinyin, __destruct);
 PHP_METHOD(Pinyin, loadDict);
 PHP_METHOD(Pinyin, convert);
 PHP_METHOD(Pinyin, multiConvert);
-PHP_METHOD(Pinyin, exact);
+PHP_METHOD(Pinyin, exactConvert);
 
 /* PHP_METHOD(Pinyin, exact2Tone); */
 /* PHP_METHOD(Pinyin, generateDict); */
