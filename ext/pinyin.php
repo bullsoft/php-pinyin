4c8147d99c991026a005a2686cfff293bfd4f16b
ideal <idealities@gmail.com>
2013-04-24 11:08:31 +0800

    modify test php script

--- ext/pinyin.php
+++ ext/pinyin.php
@@ -1,10 +1,16 @@
 <?php
-$obj = new Pinyin();
-// $obj->loadDict("/home/work/local/pinyin/dict/dict_tone.dat", Pinyin::TY_TONE_DICT);
-$obj->loadDict("/home/work/local/pinyin/dict/dict.dat", Pinyin::TY_DICT);
-// $obj->loadDict("/home/work/local/pinyin/dict/dyz_tone.dat", Pinyin::DYZ_TONE_DICT);
-$obj->loadDict("/home/work/local/pinyin/dict/dyz.dat", Pinyin::DYZ_DICT);
-$obj->loadDict("/home/work/local/pinyin/dict/duoyong.dat", Pinyin::DY_DICT);
-$obj->loadDict("/home/work/local/pinyin/dict/dz_pro.dat", Pinyin::BME_DICT);
+$user = get_current_user();
+$obj  = new Pinyin();
+
+//$obj->loadDict("/home/work/local/pinyin/dict/dict_tone.dat", Pinyin::TY_TONE_DICT);
+$obj->loadDict("/home/$user/local/pinyin/dict/dict.dat", Pinyin::TY_DICT);
+//$obj->loadDict("/home/work/local/pinyin/dict/dyz_tone.dat", Pinyin::DYZ_TONE_DICT);
+$obj->loadDict("/home/$user/local/pinyin/dict/dyz.dat", Pinyin::DYZ_DICT);
+$obj->loadDict("/home/$user/local/pinyin/dict/duoyong.dat", Pinyin::DY_DICT);
+$obj->loadDict("/home/$user/local/pinyin/dict/dz_pro.dat", Pinyin::BME_DICT);
+// var_dump($obj);
+
 var_dump($obj->convert(iconv("UTF-8", "GBK", "重庆重量")));
-?>
+var_dump($obj->multiConvert(array(iconv("UTF-8", "GBK", "重庆"))));
+var_dump($obj->multiConvert(array(iconv("UTF-8", "GBK", "重庆"), iconv("UTF-8", "GBK", "重量"))));
+var_dump($obj->exactConvert(iconv("UTF-8", "GBK", "重庆重量")));
